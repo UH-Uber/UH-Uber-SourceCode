c3141fc08244605a5dc734c3c5574937d8657bfe
lyons811 <lyonws@hawaii.edu>
2024-11-21 16:38:07 -1000

    Made it so only hawaii.edu are allowed to signup.s

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -14,3 +14,18 @@ export const EditStuffSchema = Yup.object({
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
 });
+
+
+export const SignUpSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Email is required')
+    .email('Email is invalid')
+    .matches(/@hawaii\.edu$/, 'Only @hawaii.edu email addresses are allowed'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters')
+    .max(40, 'Password must not exceed 40 characters'),
+  confirmPassword: Yup.string()
+    .required('Confirm Password is required')
+    .oneOf([Yup.ref('password'), ''], 'Confirm Password does not match'),
+});
